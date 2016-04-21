9fd3859227ff1c77dae15c1cac4702b7b973dc41
fade2g <holger.heymanns@gmail.com>
2016-04-21 21:52:58 +0200

    Adding a dummy component to get used to the seed project. works nice.

---
+++ src/client/app/Timers/components/intro.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'vt-timers-intro',
+  templateUrl: 'app/Timers/components/intro.component.html',
+  styleUrls: ['app/Timers/components/intro.component.css']
+})
+export class TimersIntroComponent{}
---
+++ src/client/app/Timers/index.ts
@@ -0,0 +1,1 @@
+export * from './components/intro.component';
